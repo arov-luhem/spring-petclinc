d96a207ac970792ca2f5d0aa622f99b7b1522cb3
mvora9 <mehulv826@gmail.com>
2018-07-30 15:32:12 -0400

    Initial Spring commit

---
+++ src/main/java/guru/springframework/sfgpetclinc/SfgPetClincApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClincApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClincApplication.class, args);
+    }
+}
